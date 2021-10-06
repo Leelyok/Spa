27892943595d0b6e846a288ae09e32b36460f02c
Lily <liliya.zapasnaya@gmail.com>
2021-10-06 10:24:04 +0300

    add animation on scroll

--- scripts.js
+++ scripts.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
         $(this).toggleClass("active");
         $(".mobile-menu").fadeToggle()
     });
+
+
+    //Animate scroll - activate
+
+    AOS.init({
+        duration: 500   // values from 0 to 3000
+    });
+
 });
